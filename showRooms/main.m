9dbf7307efc304cb284b1322fef6ab74c8248af8
IM Hoang Thai <hoangthai95ne@gmail.com>
2016-03-24 07:10:02 +0700

    Initial Commit

---
+++ showRooms/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  showRooms
+//
+//  Created by Hoàng Thái on 3/24/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
